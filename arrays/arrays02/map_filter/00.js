422b2dcf3d6845e550fdb3275f7ebcb6fcfb88a6
burak-ozturkk005 <burak.ozturkk05@gmail.com>
2026-07-03 17:51:05 +0300

    map-filter upt

--- arrays/arrays02/map_filter/00.js
+++ arrays/arrays02/map_filter/00.js
@@ -12,3 +12,17 @@ console.log(upperNames);
 let arr1 = [1,2,3,4,5];
 let arr2 = [3,4,6];
 
+//Map soru1: RPm değerini 1000 e bölünmüş halde yeni bir diziye çevir.
+const rpmler = [3000,5500,8000,9000];
+const bol_rpm = rpmler.map(rpm => rpm / 1000);
+console.log(bol_rpm);
+const hizlar = [45,120,86,152,60,170];
+//Bu dizideki 100 km/h üzreindeki hızları filtrele.
+const fltrelenmis = hizlar.filter(hiz => hiz >100);
+console.log(fltrelenmis);
+//map + filter 
+const motorlar = [{model:"CBR600RR",cc:599},{model:"Mt-09",cc:890},{model:"YZF-R6",cc:599},{model:"Panigale V4",cc:1103}];
+let lowcc = motorlar.filter(motor => motor.cc < 600);
+console.log(lowcc);
+const maplanmıs = lowcc.map(alt => alt.model );
+console.log(maplanmıs);
---
+++ practices/stars/script.js
@@ -0,0 +1,7 @@
+let satir = "";
+
+for(let i = 0; i< satir;i++)
+{
+   satir += "*";
+}
+console.log(satir);
