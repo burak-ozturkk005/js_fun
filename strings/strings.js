e079840ecf42d5a6d5e225e6847d4d0090f31a6d
burak-ozturkk005 <burak.ozturkk05@gmail.com>
2026-07-18 16:55:08 +0300

    strings

--- strings/strings.js
+++ strings/strings.js
@@ -3,9 +3,25 @@ const job = "Programmer";
 const birthYear = 1923;
 const burak ="I'm" + firstName + ', a' + job + " " + birthYear;
 console.log(burak);
-const burakNew = `I a ${firstName}, a`; // ters kesme işareti 
-console.log(burakNew); 
-console.log(burakNew.length); // returns length
-console.log(burakNew.charAt(0)); //returns at 0. index
-console.log(burakNew.at(-1)); // r eturns last index
-console.log(burakNew.includes("Dünya"));// if includes indicated word
+const burakNew = `I a ${firstName}, a`;
+let myString = "I'm here";
+console.log(myString);
+
+let yourString = 'The book I read said, "Dr. ALmanaco was a great astrologer."';
+console.log(yourString);
+console.log("24" - "20" + "4");
+console.log("24" - "20" + 4);
+firstName.length
+function calculateLength(data) {
+    let strLength = data.length;
+    console.log(strLength);
+}
+
+calculateLength("JS is used to create web applications.");
+calculateLength("This is a string, calculate its length. ");
+let firstStr = "Hello!";
+let secStr = " world";
+
+console.log(firstStr + secStr, firstStr * secStr);
+let text = "JavaScript is awesome. ";
+console.log(text.length);
